e32e9e1dbb47f16711bfa7721f1b6bbba20a0594
Elizaveta <lurovaj@gmail.com>
2023-09-30 21:50:12 +0300

    contacts and career pages styling

--- src/pages/contacts/Contacts.tsx
+++ src/pages/contacts/Contacts.tsx
@@ -1,16 +1,17 @@
 import "./Contacts.scss"
-import Map from "./sections/map/Map"
-import Contact from "./sections/contact/Contact"
+import { useSelector } from "react-redux"
 import DineWithUs from "./sections/dine-with-us/DineWithUs"
+import Contact from "./sections/contact/Contact"
+import Map from "./sections/map/Map"
 
 export default function Contacts() {
 
-
+    const { isDark } = useSelector((state: any) => state.theme)
 
     return (
         <>
             <main className="contacts">
-                <h1 className="contacts__title">contact</h1>
+                <h1 className={`contacts__title ${isDark && 'contacts__title--dark'}`}>contact</h1>
                 <Contact />
                 <Map />
             </main>
